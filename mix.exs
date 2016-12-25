8e6a64bfbaa1afb63ff0d49ba8ea9431a5cff565
ikeikeikeike <jp.ne.co.jp@gmail.com>
2016-12-25 23:40:42 +0900

    upload to hex

--- mix.exs
+++ mix.exs
@@ -1,6 +1,10 @@
 defmodule Rdtype.Mixfile do
   use Mix.Project
 
+  @description """
+  Calling Redis Data Types in easily way
+  """
+
   def project do
     [app: :rdtype,
      name: "RDTYPE",
@@ -8,7 +12,10 @@ defmodule Rdtype.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     description: @description,
+     package: package(),
+     deps: deps(),
+   ]
   end
 
   # Configuration for the OTP application
@@ -30,7 +37,20 @@ defmodule Rdtype.Mixfile do
   defp deps do
     [
       {:redix, ">= 0.0.0"},
+
       {:poison, ">= 0.0.0", only: :test},
+      {:credo, "~> 0.5", only: [:dev, :test]},
+      {:earmark, ">= 0.0.0", only: :dev},
+      {:ex_doc, "~> 0.10", only: :dev},
     ]
   end
+
+  defp package do
+    [
+      maintainers: ["Tatsuo Ikeda / ikeikeikeike"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/ikeikeikeike/rdtype"},
+    ]
+  end
+
 end
